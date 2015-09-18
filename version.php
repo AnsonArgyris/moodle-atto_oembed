b20c51e228b84fbad17b1c1bc822c00756c6602b
ewappis <erich.wappis@uni-graz.at>
2015-09-18 12:28:17 +0200

    version updated

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015091600;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2015091800;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014051205;	// Requires this Moodle version.
 $plugin->component = 'atto_oembed';  	// Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_BETA;
